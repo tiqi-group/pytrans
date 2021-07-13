1a35b83b34698de0efa11ebc41d41d13b885560f
cmordini <cmordini@phys.ethz.ch>
2021-07-13 11:57:24 +0200

    Initial commit on pytrans2
    Former-commit-id: 41ec61a96fc15d6dc18677beecb77900d89cd08c

--- pytrans/__version__.py
+++ pytrans/__version__.py
@@ -7,8 +7,8 @@ Information about the current version of the package.
 
 __title__ = 'pytrans'
 __description__ = 'Transport waveform-related generation and analysis utilities'
-__version__ = '2.0.0a'
+__version__ = '2.0.0'
 __author__ = 'tiqi'
 __author_email__ = ''
 __license__ = 'MIT'
-__url__ = 'https://gitlab.phys.ethz.ch/tiqi-projects/pytrans.git'
+__url__ = 'https://gitlab.phys.ethz.ch/tiqi-projects/pytrans2.git'
