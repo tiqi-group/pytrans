d2b4bd77beb085c5515a189942aa58059658b664
cmordini <cmordini@phys.ethz.ch>
2021-10-05 23:33:50 +0200

    Refactor cryo_analysis
    Former-commit-id: 341dd265cb010164c7b3b3444e3e8244a26cbd06

--- pytrans/utils/cryo_analysis.py
+++ pytrans/utils/cryo_analysis.py
@@ -72,6 +72,9 @@ def analyse_pot(vv, r0, electrode_indices, Vrf, Omega_rf, axes=None, roi=None):
     H = tot_hessian_ps(x1, y1, z1, *f_args)
 
     h, vs = np.linalg.eig(H)
+    ix = np.argsort(h)
+    h = h[ix]
+    vs = vs[:, ix]
     freqs = curv_to_freq(h) * 1e-6
 
     with np.printoptions(suppress=True):
@@ -102,7 +105,7 @@ def analyse_pot(vv, r0, electrode_indices, Vrf, Omega_rf, axes=None, roi=None):
     f1, f2 = freqs[[1, 2]]
     f0 = np.sqrt(f1 * f2)
 
-    angle = np.arccos(v2 @ [1, 0]) * 180 / np.pi
+    angle = np.arctan(v2[1] / v2[0]) * 180 / np.pi
     print(f"Tilt angle of mode 2 ({freqs[2]:.2f}): {angle:.2f}°")
 
     tr = fig.dpi_scale_trans + transforms.ScaledTranslation(y1 * 1e6, z1 * 1e6, ax_im.transData)
@@ -117,3 +120,62 @@ def analyse_pot(vv, r0, electrode_indices, Vrf, Omega_rf, axes=None, roi=None):
     ax_im.add_patch(a2)
 
     return res
+
+
+def analyse_pot_data(vv, r0, electrode_indices, Vrf, Omega_rf, roi=None):
+    roi = __roi if roi is None else roi
+
+    f_args = (vv, electrode_indices, Vrf, Omega_rf)
+
+    def fun3(xyz):
+        return tot_potential_ps(*xyz, *f_args)
+
+    _roi = []
+    for lim in roi:
+        lim = lim if isinstance(lim, (int, float)) else min(lim)
+        _roi.append(lim)
+
+    bounds = [(-r * 1e-6 + x, r * 1e-6 + x) for r, x in zip(_roi, r0)]
+
+    res = minimize(fun3, r0, method='TNC', bounds=bounds, options=dict(accuracy=1e-2))
+
+    print("Offset from r0 [um]")
+    print((res.x - r0) * 1e6)
+    x1, y1, z1 = res.x
+    # print(res)
+
+    E = tot_gradient_ps(x1, y1, z1, *f_args)
+    H = tot_hessian_ps(x1, y1, z1, *f_args)
+
+    h, vs = np.linalg.eig(H)
+    ix = np.argsort(h)
+    h = h[ix]
+    vs = vs[:, ix]
+    freqs = curv_to_freq(h) * 1e-6
+
+    with np.printoptions(suppress=True):
+        print('Gradient')
+        print(E)
+        print(field_to_shift(E) * 1e6)
+        print('Hessian')
+        print(H)
+        print(curv_to_freq(H) * 1e-6)
+        print('Eigenvalues [MHz]')
+        print(freqs)
+        print('Eigenvectors')
+        print(vs)
+
+    v2 = vs[1:, 2]
+    angle = np.arctan(v2[1] / v2[0]) * 180 / np.pi
+    print(f"Tilt angle of mode 2 ({freqs[2]:.2f}): {angle:.2f}°")
+
+    results = dict(
+        x=res.x[0],
+        y=res.x[1],
+        z=res.x[2],
+        fx=freqs[0],
+        fy=freqs[1],
+        fz=freqs[2],
+        angle=angle
+    )
+    return results
