5e055ebd0564da507caa78b48d7c9cf3e503c1e3
cmordini <cmordini@phys.ethz.ch>
2022-02-12 02:45:32 +0100

    Abstract trap based on functions
    Former-commit-id: a3249641cc21c33d54792a4727ae163d34099dde

--- pytrans/trap_model/abstract_trap.py
+++ pytrans/trap_model/abstract_trap.py
@@ -3,12 +3,7 @@
 #
 # Created: 01-2021 - Carmelo Mordini <carmelo> <cmordini@phys.ethz.ch>
 
-"""
-Module docstring
-
-"""
-from typing import Union, List
-from numpy.typing import ArrayLike
+import numpy as np
 from abc import ABC, abstractmethod
 
 import logging
@@ -16,29 +11,93 @@ logger = logging.getLogger(__name__)
 
 
 class AbstractTrap(ABC):
-    """ I have to define here:
-    - transport axis (alias to x)
-    - moments and their derivatives
-    - pseudo_potential and its derivatives
+    """This class does nothing, but it will force the implementation
+    of some properties and methods in trap models that are used in the Objectives
+
+    Required:
+    - functions for 3d dc potentials and derivatives
+    - functions for 3d RF pseudopotential and derivatives
+
+    Optional?:
     - num_electrodes
-    - electrode mapping
+    - electrode - channel mapping, for waveforms
     - electrode_constrains (min, max, slew rate)
     - filters?
     """
-    num_electrodes: int
+    __required_attributes = ['n_dc']
 
-    @property
+    def __new__(cls):
+        print("Creating a new trap")
+        for name in cls.__required_attributes:
+            if not hasattr(cls, name):
+                raise TypeError(f"Can't instantiate abstract class {cls.__name__} with abstract attributes {','.join(cls.__required_attributes)}")
+
+        return super().__new__(cls)
+
     @abstractmethod
-    def x(self) -> ArrayLike:
-        '''transport axis'''
+    def dc_potentials(self, x, y, z):
+        """
+        Returns:
+            out: array_like, shape: (n_dc,) + x.shape
+        """
         pass
 
     @abstractmethod
-    def dc_potential(self, derivatives: Union[int, str, List[str]], electrode_indices: Union[slice, List[int], ArrayLike]) -> ArrayLike:
-        '''dc potentials: shape (n_electrodes, derivative, len(x))'''
+    def dc_gradients(self, x, y, z):
+        """
+        Returns:
+            out: array_like, shape: (n_dc, 3) + x.shape
+        """
         pass
 
     @abstractmethod
-    def pseudo_potential(self, derivatives: Union[int, str, List[str]]) -> ArrayLike:
-        '''pseudo potential: shape (derivative, len(x))'''
+    def dc_hessians(self, x, y, z):
+        """
+        Returns:
+            out: array_like, shape: (n_dc, 3, 3) + x.shape
+        """
         pass
+
+    @abstractmethod
+    def pseudo_potential(self, x, y, z):
+        """Pseudopotential from RF
+
+        Returns:
+            out: array_like, shape: x.shape
+        """
+        pass
+
+    @abstractmethod
+    def pseudo_gradient(self, x, y, z):
+        """Pseudopotential gradient
+
+        Returns:
+            out: array_like, shape: (3,) + x.shape
+        """
+        pass
+
+    @abstractmethod
+    def pseudo_hessian(self, x, y, z):
+        """Pseudopotential curvatures
+
+        Returns:
+            out: array_like, shape: (3, 3) + x.shape
+        """
+        pass
+
+    def potential(self, voltages, x, y, z):
+        return np.einsum('i,i...', voltages, self.dc_potentials(x, y, z)) + self.pseudo_potential(x, y, z)
+
+    def gradient(self, voltages, x, y, z):
+        return np.einsum('i,i...', voltages, self.dc_gradients(x, y, z)) + self.pseudo_gradient(x, y, z)
+
+    def hessian(self, voltages, x, y, z):
+        return np.einsum('i,i...', voltages, self.dc_hessians(x, y, z)) + self.pseudo_hessian(x, y, z)
+
+
+if __name__ == '__main__':
+
+    class Trap(AbstractTrap):
+        n_dc = 10
+        pass
+    trap = Trap()
