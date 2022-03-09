8a856ae7559a11c10cbe38b879747b3455508797
cmordini <cmordini@phys.ethz.ch>
2022-03-09 16:03:51 +0100

    Pseudo option in abstract_model and analysis functions
    Former-commit-id: 470cc91dcfda5109e192c7f15a3eb0ff902d714c

--- pytrans/analysis/analysis.py
+++ pytrans/analysis/analysis.py
@@ -10,8 +10,7 @@ Module docstring
 
 import numpy as np
 from numpy.typing import ArrayLike
-
-from pytrans.plotting import plot3d_potential, plot3d_make_layout
+from pytrans.plotting import plot3d_potential, plot3d_make_layout, plot_fields_curvatures
 from pytrans.conversion import curv_to_freq, field_to_shift
 from pytrans.ions import Ca40
 from pytrans.utils.timer import timer
@@ -40,12 +39,37 @@ def analyse_hessian(H):
     return h, vs, angle
 
 
+def analyse_fields_curvatures(trap: AbstractTrap, voltages: ArrayLike, x, y=None, z=None, pseudo=True, plot=True):
+    y = getattr(trap, 'y0', 0) if y is None else y
+    z = getattr(trap, 'z0', 0) if z is None else z
+    print('--------------\n' + Fore.YELLOW + "Analyse curvatures along trajectory")
+    fields = np.empty(x.shape + (3,))
+    modes = np.empty(x.shape + (3,))
+    angle = np.empty_like(x)
+    if voltages.ndim == 1:
+        voltages = np.tile(voltages, (len(x), 1))
+    for j, x1 in enumerate(tqdm(x)):
+        E = trap.gradient(voltages[j], x1, y, z, pseudo=pseudo)
+        H = trap.hessian(voltages[j], x1, y, z, pseudo=pseudo)
+        h, _, angle[j] = _eig_hessian(H)
+        fields[j] = E
+        modes[j] = curv_to_freq(h, ion=trap.ion)
+    results = dict(
+        fields=fields,
+        modes=modes,
+        angle=angle
+    )
+    if plot:
+        results['fig'], results['axes'] = plot_fields_curvatures(x, fields, modes, angle)
+    return results
+
+
 def analyse_potential_data(trap: AbstractTrap, voltages: ArrayLike, r0: ArrayLike,
                            roi=None, find_3dmin=True, minimize_options=dict()):
     roi = __roi if roi is None else roi
 
     def fun3(xyz):
-        return trap.potential(voltages, *xyz)
+        return trap.potential(voltages, *xyz, pseudo=pseudo)
 
     print('--------------\n' + Fore.YELLOW + "Analyse potential")
     if find_3dmin:
@@ -68,8 +92,8 @@ def analyse_potential_data(trap: AbstractTrap, voltages: ArrayLike, r0: ArrayLik
         x1, y1, z1 = r0
         v = fun3(r0)
 
-    E = trap.gradient(voltages, x1, y1, z1)
-    H = trap.hessian(voltages, x1, y1, z1)
+    E = trap.gradient(voltages, x1, y1, z1, pseudo=pseudo)
+    H = trap.hessian(voltages, x1, y1, z1, pseudo=pseudo)
 
     h, vs, angle = analyse_hessian(H)
 
@@ -119,7 +143,6 @@ def analyse_potential(trap: AbstractTrap, voltages: ArrayLike, r0: ArrayLike, pl
     freqs = res['fx'], res['fy'], res['fz']
     f1 = res['fun']
     vs = res['eigenvectors']
-    curv_x = res['eigenvalues'][0]
     angle = res['angle']
 
     if plot:
@@ -128,9 +151,10 @@ def analyse_potential(trap: AbstractTrap, voltages: ArrayLike, r0: ArrayLike, pl
     ax_x, ax_y, ax_z, ax_im, ax0 = axes
     fig = ax_x.figure
 
-    _range = np.linspace(-roi[0], roi[0], 50) * 1e-6 / 4
-    xx1 = _range + x1
-    ax_x.plot(xx1 * 1e6, 0.5 * curv_x * (xx1 - x1)**2 + f1)
+    # _range = np.linspace(-roi[0], roi[0], 50) * 1e-6 / 4
+    # curv_x = res['eigenvalues'][0]
+    # xx1 = _range + x1
+    # ax_x.plot(xx1 * 1e6, 0.5 * curv_x * (xx1 - x1)**2 + f1)
 
     marker_kw = dict(marker='o', mfc='r', mec='r')
 
@@ -142,7 +166,7 @@ def analyse_potential(trap: AbstractTrap, voltages: ArrayLike, r0: ArrayLike, pl
     v1 = vs[1:, 1]
     v2 = vs[1:, 2]
     f1, f2 = freqs[1], freqs[2]
-    f0 = np.sqrt(f1 * f2)
+    f0 = np.sqrt(abs(f1 * f2))
 
     tr = fig.dpi_scale_trans + transforms.ScaledTranslation(y1 * 1e6, z1 * 1e6, ax_im.transData)
 
@@ -154,5 +178,8 @@ def analyse_potential(trap: AbstractTrap, voltages: ArrayLike, r0: ArrayLike, pl
     ax_im.add_patch(a1)
     a2 = mpatches.Arrow(0, 0, *v2 * f0 / f2, width=0.2, transform=tr, color='C1')
     ax_im.add_patch(a2)
+
+    res['fig'] = fig
+    res['axes'] = axes
 
     return res
