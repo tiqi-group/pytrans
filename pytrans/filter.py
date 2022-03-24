f18ba6a833094d1828b9b2933a294d224ade8c8b
cmordini <cmordini@phys.ethz.ch>
2022-03-24 19:12:14 +0100

    Include filters in solver
    Former-commit-id: 7252ef66b2398c98c9055fd4adb5cc64618017d7

---
+++ pytrans/filter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# Created: 03/2022
+# Author: Carmelo Mordini <cmordini@phys.ethz.ch>
+
+
+import numpy as np
+import scipy.signal as sg
+
+
+def pad_waveform(waveform, pad_after, pad_before=0):
+    """Extends a waveform along the time axis
+    """
+    return np.pad(waveform, ((pad_before, pad_after), (0, 0)), mode='edge')
+
+
+def lfilter_waveform(b, a, waveform):
+    """Filters a waveform using a digital filter
+    in the transfer function representation
+                        -1              -M
+            b[0] + b[1]z  + ... + b[M] z
+    Y(z) = -------------------------------- X(z)
+                        -1              -N
+            a[0] + a[1]z  + ... + a[N] z
+
+    Parameters:
+        b: array_like
+        a: array_like
+        waveform: array_like, shape (n_samples, n_electrodes)
+
+    Returns
+        filtered_waveform
+    """
+    zi = sg.lfilter_zi(b, a)
+    filtered_waveform, zf = sg.lfilter(b, a, waveform, axis=0, zi=np.outer(zi, waveform[0]))
+    return filtered_waveform
--- pytrans/solver.py
+++ pytrans/solver.py
@@ -13,6 +13,10 @@ from .abstract_model import AbstractTrap
 from .objectives import Objective
 from typing import List, Any
 
+import numpy as np
+from scipy import signal as sg
+from scipy.linalg import convolution_matrix
+
 from tqdm import tqdm
 
 
@@ -20,6 +24,7 @@ def solver(trap: AbstractTrap,
            step_objectives: List[List[Objective]],
            global_objectives: List[Objective],
            extra_constraints: List[Any] = None,
+           trap_filter=None,
            solver="MOSEK", start_value=None, verbose=False):
     """Static solver
 
@@ -32,10 +37,24 @@ def solver(trap: AbstractTrap,
         """
 
     # static voltage cost
-    shape = (len(step_objectives), trap.n_electrodes)
-    waveform = cx.Variable(shape=shape, name="waveform")
-    costs = []  # type: ignore
-    cstr = []   # type: ignore
+    n_steps = len(step_objectives)
+    costs = []
+    cstr = []
+    if trap_filter is None:
+        waveform = cx.Variable(shape=(n_steps, trap.n_electrodes), name="waveform")
+        waveform0 = waveform
+    else:
+        # this might be moved in the .trap_filter module
+        b, a, dt = trap_filter
+        _t, h = sg.dimpulse((b, a, dt))
+        h = np.squeeze(h)
+        m = len(h) - 1
+        waveform0 = cx.Variable(shape=(n_steps + m, trap.n_electrodes), name="waveform")
+        for j in range(m):
+            cstr.append(waveform0[j] == waveform0[m])
+        M = convolution_matrix(h, waveform0.shape[0], mode='valid')
+        waveform = M @ waveform0
+        waveform0 = waveform0[m:]
 
     for voltages, ci in zip(waveform, tqdm(step_objectives, desc="Compiling step objectives")):
         for cj in ci:
@@ -45,10 +64,13 @@ def solver(trap: AbstractTrap,
                 cstr.extend(cj.constraint(trap, voltages))
 
     for c in global_objectives:
+        # if c.__class__.__name__ == "SlewRateObjective":
+        #     costs.extend(c.objective(trap, waveform0))
+        # else:
         if c.constraint_type is None:
-            costs.extend(c.objective(trap, waveform))
+            costs.extend(c.objective(trap, waveform0))
         else:
-            cstr.extend(c.constraint(trap, waveform))
+            cstr.extend(c.constraint(trap, waveform0))
 
     if extra_constraints is not None:
         cstr.extend(extra_constraints)
@@ -64,4 +86,4 @@ def solver(trap: AbstractTrap,
     #     final_costs.append({f"{j}_{cj.__class__.__name__}": [c for c in cj.objective(trap, voltages)] for j, cj in enumerate(ci)})
     # final_costs.append({f"{j}_global_{cj.__class__.__name__}": [c for c in cj.objective(trap, waveform)] for j, cj in enumerate(global_objectives)})
 
-    return waveform, final_costs
+    return waveform0, final_costs
